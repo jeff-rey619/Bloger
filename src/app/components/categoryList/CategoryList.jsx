cbe7e7ba1b3f30310531bcf7345297c9a23449f1
jeff rey <geffrey1182002@gmail.com>
2024-05-15 08:18:33 +0530

    css changes

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -10,12 +10,33 @@ function CategoryList() {
                 Popular Categories
             </h1>
             <div className={styles.categories}>
-                <div className={styles.category}>
+             
                     
-                    <Link href="/blog?cat=style">
-                        <Image src="/style.png" alt='' width={32} height={32} className={styles.image} />
+                    <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt='' width={32} height={32} className={styles.image} />
+                    Style
+                </Link>
+
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/food.png" alt='' width={32} height={32} className={styles.image} />
+                    Food
+                </Link>
+                
+                  <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/travel.png" alt='' width={32} height={32} className={styles.image} />
+                    Travel
+                </Link>
+                
+                  <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/culture.png" alt='' width={32} height={32} className={styles.image} />
+                    Culture
+                </Link>
+                
+                  <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/coding.png" alt='' width={32} height={32} className={styles.image} />
+                    Coding
                     </Link>
-                </div>
+    
             </div>
         </div>
     );
